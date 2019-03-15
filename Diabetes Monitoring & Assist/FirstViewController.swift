37be6c10822e7186d411751486f28c5e16cde6ed
Wyte Krongapiradee <wkrongap@uci.edu>
2019-03-15 02:35:27 -0700

    Initial Commit

---
+++ Diabetes Monitoring & Assist/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Diabetes Monitoring & Assist
+//
+//  Created by Kay Lab on 3/15/19.
+//  Copyright © 2019 UC Irvine. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Diabetes Monitoring & Assist/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Diabetes Monitoring & Assist
+//
+//  Created by Kay Lab on 3/15/19.
+//  Copyright © 2019 UC Irvine. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
