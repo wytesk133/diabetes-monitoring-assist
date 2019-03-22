5538f0a853a5c250ad4941435356ae1867397243
Jason Padilla <jpadill3@uci.edu>
2019-03-21 23:41:14 -0700

    Add recommendations and notifications

--- Diabetes Monitoring & Assist/RecommendationsViewController.swift
+++ Diabetes Monitoring & Assist/RecommendationsViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import UserNotifications
 
 class RecommendationsViewController: UIViewController {
 
+    @IBAction func night(_ sender: Any) {
+        let content2 = UNMutableNotificationContent()
+        content2.title = "Night Notification"
+        content2.subtitle = "It is time!"
+        content2.body = "Based on your current statistics you should workout more and eat carbs with less calories. Also get enough sleep!"
+        content2.badge = 1
+        
+        let trigger2 = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request2 = UNNotificationRequest(identifier: "timerDone", content: content2, trigger: trigger2)
+        
+        UNUserNotificationCenter.current().add(request2, withCompletionHandler: nil)
+    }
+    @IBAction func action(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "Morning Notification"
+        content.subtitle = "It is time!"
+        content.body = "Good Morning please make sure to check your glucose levels and take your medication."
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: "timerDone", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        
+       
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge], completionHandler: {didAllow, error in})
+        
         // Do any additional setup after loading the view.
     }
     
